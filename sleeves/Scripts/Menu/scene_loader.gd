6b953bdb50b9095734a287c434e5b455a3f43d05
JP <josiahp13579@gmail.com>
2024-11-30 22:06:53 -0600

    made some simple changes

--- sleeves/Scripts/Menu/scene_loader.gd
+++ sleeves/Scripts/Menu/scene_loader.gd
@@ -3,4 +3,4 @@ extends Node
 
 
 func _on_start_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/level_select.tscn")
+	get_tree().change_scene_to_file("res://Level_select/level_select.tscn")
